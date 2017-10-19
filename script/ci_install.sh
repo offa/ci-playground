ff7b8be03f7a5ab2cdbd1f2ad05d1d475cfe1c4f
offa <bm-dev@yandex.com>
2017-10-19 16:05:18 +0200

    Remove Sanitizer options as they are not used.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -17,14 +17,11 @@ if [[ -z "$BUILD_32_BITS" ]]; then
   export BUILD_32_BITS=OFF && echo disabling 32 bit build
 fi
 
-# Build and install libc++ (Use unstable ABI for better sanitizer coverage)
 mkdir llvm-build && cd llvm-build
 cmake -DCMAKE_C_COMPILER=${CC} \
         -DCMAKE_CXX_COMPILER=${CXX} \
         -DCMAKE_BUILD_TYPE=RelWithDebInfo \
         -DCMAKE_INSTALL_PREFIX=/usr \
-        -DLIBCXX_ABI_UNSTABLE=ON \
-        -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
         -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
         ../llvm-source
 make cxx -j4
