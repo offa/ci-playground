d0080e613630fc91c25a95c6a3e1f90213dd93b0
offa <bm-dev@yandex.com>
2017-10-19 14:54:55 +0200

    Clone the 5.0 release branch instead of master.

--- script/ci_install.sh
+++ script/ci_install.sh
@@ -7,9 +7,9 @@ sudo ./install-cmake.sh --prefix=/usr/local --skip-license
 
 
 # Checkout LLVM sources
-git clone --depth=1 https://github.com/llvm-mirror/llvm.git llvm-source
-git clone --depth=1 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
-git clone --depth=1 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
+git clone --depth=1 -b release_50 https://github.com/llvm-mirror/llvm.git llvm-source
+git clone --depth=1 -b release_50 https://github.com/llvm-mirror/libcxx.git llvm-source/projects/libcxx
+git clone --depth=1 -b release_50 https://github.com/llvm-mirror/libcxxabi.git llvm-source/projects/libcxxabi
 
 # Setup libc++ options
 if [ -z "$BUILD_32_BITS" ]; then
@@ -18,13 +18,15 @@ fi
 
 # Build and install libc++ (Use unstable ABI for better sanitizer coverage)
 mkdir llvm-build && cd llvm-build
-cmake -DCMAKE_C_COMPILER=${C_COMPILER} -DCMAKE_CXX_COMPILER=${COMPILER} \
-      -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_INSTALL_PREFIX=/usr \
-      -DLIBCXX_ABI_UNSTABLE=ON \
-      -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
-      -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
-      ../llvm-source
-make cxx -j2
+cmake -DCMAKE_C_COMPILER=${C_COMPILER} \
+        -DCMAKE_CXX_COMPILER=${COMPILER} \
+        -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+        -DCMAKE_INSTALL_PREFIX=/usr \
+        -DLIBCXX_ABI_UNSTABLE=ON \
+        -DLLVM_USE_SANITIZER=${LIBCXX_SANITIZER} \
+        -DLLVM_BUILD_32_BITS=${BUILD_32_BITS} \
+        ../llvm-source
+make cxx -j4
 sudo make install-cxxabi install-cxx
 cd ../
 
