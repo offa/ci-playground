588393cb5d9e3e4099ff110169d15099a610531f
offa <bm-dev@yandex.com>
2017-11-07 20:37:04 +0100

    Don't use C++17 specific features on compilers which don't fully support it yet.

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,17 @@
 #include <cstddef>
+#include <cstdio>
+
+
+#ifdef USE_COMPAT
+# pragma message "* Compat *"
 
 int main()
 {
+#ifndef USE_COMPAT
     std::byte b{7};
+#else
+    std::uint8_t b{7};
+#endif /* USE_COMPAT */
 
 
     return 0;
--- script/ci_build.sh
+++ script/ci_build.sh
@@ -2,11 +2,16 @@
 
 set -ex
 
+if (( $# == 1 )); then
+    echo "Use compat"
+    BUILD_PARAMS="-DUSE_COMPAT=ON"
+fi
+
 git --version
 cmake --version
 ${CC} --version
 ${CXX} --version
 
 mkdir build && cd build
-cmake ..
+cmake ${BUILD_PARAMS} ..
 make
