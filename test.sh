85cbafe5abbdd089fea438195739a4f5dc3d2a73
offa <bm-dev@yandex.com>
2025-03-27 07:43:39 +0100

    Test

--- test.sh
+++ test.sh
@@ -3,5 +3,8 @@
 python --version
 
 pip install -U conan
+conan profile detect
+echo "tools.cmake.cmaketoolchain:generator=Ninja" >> ~/.conan2/global.conf
+
 
 conan install --build=missing --output-folder=_dist .
