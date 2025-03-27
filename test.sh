f68efabfee9ed5ac3e33b5e6bc41ab166930bc78
offa <bm-dev@yandex.com>
2025-03-27 08:36:33 +0100

    Test

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 python --version
 
 pip install -U conan
@@ -7,4 +9,9 @@ conan profile detect
 echo "tools.cmake.cmaketoolchain:generator=Ninja" >> ~/.conan2/global.conf
 
 
-conan create --build=missing --version=1.84.0 recipes/boost/all
+conan create \
+    --build=missing \
+    --version=1.84.0 \
+    -o "boost/*:without_python"=False \
+    -o "boost/*:without_numpy"=True \
+    recipes/boost/all
