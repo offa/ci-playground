348c478911dcbebddf9162dfaccfd06d092c1ab8
offa <8887756+offa@users.noreply.github.com>
2026-03-13 18:53:26 +0100

    Test

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+git clone --depth=1 https://github.com/catchorg/Catch2.git catch2
+
+mkdir -p catch2/build
+cd catch2/build
+
+cmake -DCATCH_DEVELOPMENT_BUILD=ON ..
+make -j
