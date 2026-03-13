63275b87548cfe3dd048aa9dc527494879b946d8
offa <8887756+offa@users.noreply.github.com>
2026-03-13 19:24:51 +0100

    Test

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ git clone --depth=1 https://github.com/catchorg/Catch2.git catch2
 mkdir -p catch2/build
 cd catch2/build
 
-cmake -DCATCH_DEVELOPMENT_BUILD=ON ..
+cmake -DCATCH_DEVELOPMENT_BUILD=ON -DCATCH_ENABLE_WERROR=OFF ..
 make -j
